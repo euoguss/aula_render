369a519bf8cf6036c197d952e705ea097b700d02
Gustavo Dias <128855647+euoguss@users.noreply.github.com>
2024-09-04 19:40:21 -0300

    Delete Aula003 directory

--- Aula003/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, url_for, render_template
-from routes.port import port_route
-app = Flask(__name__)
-
-app.register_blueprint(port_route)
-
-
-app.run(debug=True)
-
--- Aula003/routes/port.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, Blueprint
-
-port_route = Blueprint('port', __name__)
-
-@port_route.route("/")
-def form():
-    return render_template("index.html")
-    ...
